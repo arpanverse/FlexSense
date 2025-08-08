5969fe5e18eca5cc8a70b0ec523193a61d913a39
Arpan Das <arpandas@M1.local>
2025-08-08 19:42:03 +0530

    buildathon initial commit

---
+++ FlexSenseMVP/FlexSenseMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexSenseMVPApp.swift
+//  FlexSenseMVP
+//
+//  Created by Arpan Das on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FlexSenseMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
